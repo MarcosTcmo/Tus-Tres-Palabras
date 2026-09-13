5bdbd9ab71da358f6b283dc9dd8511028e94abf5
Ramirez Marcos Elias <tecmorym0@gmail.com>
2026-09-13 05:03:23 -0300

    correcion de carga de jugadores

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const form = document.getElementById('playerForm')
 form.addEventListener('submit', (event) => {
     event.preventDefault()
+    
     const playerName = document.getElementById('playerName').value
     if (playerName === '') {
         alert('Por favor, ingresa n nombre valido')
         return
     }
-    players.push(playerName)
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')
+    players.push({name: playerName,color: randomColor})
     renderPlayers()
     document.getElementById('playerName').value = ''
 })
@@ -15,12 +17,13 @@ function renderPlayers() {
     const container = document.getElementById('playerContainer')
     container.innerHTML = ''
     players.forEach((player) => {
-        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')
         const card = document.createElement('section')
-        card.className = 'card mb-2'
-        card.style.backgroundColor = randomColor
+        card.className = 'card my-2 w-75 mx-auto'
+        card.style.backgroundColor = player.color
+        card.style.boxShadow = 'inset 0 0 0 5px rgba(0, 0, 0, 0.4)'
+        card.style.borderRadius = '15px'
         card.innerHTML = `<section class="card-body text-center">
-        <h5 class="card-title">${player}</5>
+        <h5 class="card-title">${player.name}</5>
         </section>`
         container.appendChild(card)
     });
